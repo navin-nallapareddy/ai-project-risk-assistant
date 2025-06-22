d45a4e54323fa01f3510b821031b930ebae59162
NNR0881 <navinkalyanreddy@icloud.com>
2025-06-22 10:45:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ app.post("/generate", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => console.log(`Server running on port ${port}`));
